17e4f07f434d112d126b718824067c7195ac7d2c
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-23 18:23:06 -0500

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -4,7 +4,7 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
   return (
     <div>
       <nav className="homenav">
-          <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
+         // <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
           <h1 id="title-white"> Tre Weathers </h1>
           <Link className="link white" to={Home}> Home </Link>
           <Link className="link white" to={Aboutme}> About Me </Link>
