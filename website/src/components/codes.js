15fd337afff46cf1cf8502ae6ea15119a1decbe2
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 16:07:57 -0500

    Update codes.js

--- website/src/components/codes.js
+++ website/src/components/codes.js
@@ -3,21 +3,19 @@ export default function Codes() {
     <div className="">
 
 <hr></hr>
-        <h2 className=""> Example 1: This Front End Website! </h2>
-        <p className="black">If it was not already assumed, I wrote the code for this website entirely myself, using a React.js frontend and correlating frameworks.
-        I used GitHub actions, yarn, and Microsoft Azure to deploy, and for data seeding I am working with MongoDB.</p>
+        <h2 className=""> Example 1: This Website! </h2>
+        <p className="black">If it was not already assumed, I wrote the code for this website myself using the React.js library for the frontend and GitHub actions, yarn, and Microsoft Azure to deploy. </p>
 
         <h2 className=""> Example 2: The Virtual Classroom </h2>
-
-        <p className="black">
-        Simply put, the Virtual Classroom / Detention is where the non-traditional students like me get sent to go supplement our clasroom education after being kicked out
-        of traditional classrooms for "poor classroom performance" so they can learn on their own time! </p>
+        <p className="black"> This was my capstone for the software engineering bootcamp where I began my studies.
+    While AI was not a part of the program, by the end I was heavily interested.
+    To incorporate this, I integrated the BERT QnA model into my website using TensorFlow backend, allowing users to extract details from the content of a given passage through q&a. 
+         </p>
 
         <h3>ToolKit</h3>
-        <p className="black">TensorFlow BERT Model from TensorFlowHub w/ Tensorflow cpu, WebGL, Node.js, React.js & Azure to deploy. I also used axios to fetch 3rd party API
-        from NASA.
-          TensorFlow models are BERT Q&A models for Natural Language Question Answering: Answer questions based off content of a given passage,
-          serving as a virtual tutor in The Virtual Classroom Detention</p>
+        <p className="black"> TensorFlow BERT Model from TensorFlowHub w/ Tensorflow cpu, WebGL, Node.js, and React.js for AI and Frontend, Azure to deploy, Axios to fetch 3rd party API
+        from NASA, and TensorFlow BERT Q&A model for Natural Language Question Answering.</p>
+          
         <a className="github" href="https://proud-plant-0981ce810.3.azurestaticapps.net/"> Site Link - click here to go to the Virtual Class! </a>
           
         <img className="exampleer" src="https://i.postimg.cc/vmQfCVr6/0954-CFAF-BFE4-42-A3-BDDF-84-C940473277-1-201-a.jpg" align="center" alt="capstone1"></img>
@@ -29,3 +27,12 @@ export default function Codes() {
       </div>
     )
   }
+
+/*
+ serving as a virtual tutor in The Virtual Classroom Detention
+ 
+Simply put, the Virtual Classroom / Detention is where the non-traditional students like me get sent to go supplement our clasroom education after being kicked out
+        of traditional classrooms for "poor classroom performance" so they can learn on their own time!
+        
+and for data seeding I am working with MongoDB
+*/
