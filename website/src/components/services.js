b12aa24062e261465ec5b8a38e99cebc05674cfd
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 13:30:22 -0500

    Update services.js

--- website/src/components/services.js
+++ website/src/components/services.js
@@ -5,12 +5,11 @@ export default function Services() {
       <div className="">
         <Navd Data="/data" Algorithms="/algorithms"  Codes="/codes" Consulting="/consulting" />
     
-          <h2 className="blue"> Tools | Technologies </h2>
+          <h2 className="blue"> Tech Stack </h2>
           
           <div className="black">
-          <h4> Go-to Full-Stack Tech Stack: React, Python, TensorFlow, Java, SQL </h4>
-          <p> Machine Learning | AI | Data: NumPy, TensorFlow, SQL, pandas, SciPy, matplotlib </p>
-          <p> Research, Development, Troubleshooting and Debugging </p>
+          <h4> "Go-to" Full-Stack: React, Python, TensorFlow, Java, SQL </h4>
+          <h4> Machine Learning | AI | Data: NumPy, TensorFlow, SQL, pandas, SciPy, matplotlib </h4>
           </div>
     
           <a className="github" href="https://github.com/TREYCSE/GitHub_Portfolio">GitHub PortFolio README.md for Engineers</a>
@@ -19,6 +18,8 @@ export default function Services() {
     )
   }
 /*
+<p> Research, Development, Troubleshooting and Debugging </p>
+ Tools | Technologies
  <h2 className="blue"> Administrative | Consulting | Management</h2>
  <p> Project planning and scope management: Microsoft Suite & Google Suite </p>
  <p> Research: experience navigating various types of databases </p>
