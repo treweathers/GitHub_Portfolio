bf9d873d623854674f1ab53a648ec06db769f385
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 10:08:57 -0500

    Update services.js

--- website/src/components/services.js
+++ website/src/components/services.js
@@ -3,8 +3,6 @@ import  Navd  from "./nav_3.js";
 export default function Services() {
   return (
       <div className="">
-        
-        <h3> click buttons below for code samples, results, summaries, visuals, and more from my completed and in-progress projects! </h3>
         <Navd Data="/data" Algorithms="/algorithms"  Codes="/codes" Consulting="/consulting" />
         <hr></hr>
         <a className="github" href="https://github.com/TREYCSE/GitHub_Portfolio">GitHub PortFolio README.md for Engineers: Click here for code samples, etc.</a>
@@ -30,3 +28,8 @@ export default function Services() {
       </div>
     )
   }
+
+/*
+        <h3> click buttons below for code samples, results, summaries, visuals, and more from my completed and in-progress projects! </h3>
+
+*/
