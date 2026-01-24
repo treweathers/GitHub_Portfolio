e7bf2ab5c00339140bc7ccbc09fa694bc6e09f7c
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 17:24:11 -0500

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -2,12 +2,14 @@ export default function Data() {
   return (
     <div className="">
           <h3> WEB SCRAPER </h3>
-          <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="github">  Web scraping tool in progress </a>
           <h4 className="black"> This basic Web Scraper is made with Scrapy in Python and the data is saved as json files.
                 I began scraping Legal data from government websites such as pulling articles from the Constitution full text online.
                 In the future when parsing data for a larger AI project, I will focus on deploying and automation with Azure PostgreSQL and GitHub Actions.
                 In addition, I will pivit to looking for websites to scrape that are better formatted and therefore better to parse through.
           </h4>
+
+          <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="github">  Click here for repo </a>
+
       </div>
     )
   }
