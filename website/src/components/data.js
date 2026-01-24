cebba52f3b1d99705e4224c4953b27a1df1dc3e4
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 17:21:07 -0500

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -3,12 +3,10 @@ export default function Data() {
     <div className="">
           <h3> WEB SCRAPER </h3>
           <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="github">  Web scraping tool in progress </a>
-          <h4 className="black"> The Web Scraper is made with Scrapy in Python and the data is saved as json files.
-                I am scraping Legal data from government websites such as pulling articles from the Constitution full text online.
-                Now that the data has been parsed, I am focusing on deploying and automation with Azure PostgreSQL and GitHub Actions.
-                In addition, looking for websites with surplus of data, rather than limited articles. In research phase continually.
-                Considering other legal data to scrape that is better formatted and therefore better to parse through.
-                SQL medical database also in progress, researching best sites to scrape and clean - stay tuned on progress! 
+          <h4 className="black"> This basic Web Scraper is made with Scrapy in Python and the data is saved as json files.
+                I began scraping Legal data from government websites such as pulling articles from the Constitution full text online.
+                In the future when parsing data for a larger AI project, I will focus on deploying and automation with Azure PostgreSQL and GitHub Actions.
+                In addition, I will pivit to looking for websites to scrape that are better formatted and therefore better to parse through.
           </h4>
       </div>
     )
@@ -18,6 +16,7 @@ export default function Data() {
 
 /*
 
+SQL medical database also in progress, researching best sites to scrape and clean - stay tuned on progress! 
           <h3> SQL database </h3>
           <a href = "https://github.com/TREYCSE/Data_SQL" className="title"> Database in progress </a>
           <h4> In progress... </h4>
