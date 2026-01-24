25fb77ad7fd946879d650a5e0a958a29e3fb200d
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 16:08:57 -0500

    Update codes.js

--- website/src/components/codes.js
+++ website/src/components/codes.js
@@ -18,8 +18,6 @@ export default function Codes() {
           
         <a className="github" href="https://proud-plant-0981ce810.3.azurestaticapps.net/"> Site Link - click here to go to the Virtual Class! </a>
           
-        <img className="exampleer" src="https://i.postimg.cc/vmQfCVr6/0954-CFAF-BFE4-42-A3-BDDF-84-C940473277-1-201-a.jpg" align="center" alt="capstone1"></img>
-
        <h3>Snapshots</h3>
        <img className="exampleer" src="https://i.imgur.com/9qRZRqA.jpg" align="center" alt="capstone1"></img>
        <img className="exampleer" src="https://i.imgur.com/QDDD5Vu.jpg" align="center" alt="capstone2"></img>
@@ -29,6 +27,9 @@ export default function Codes() {
   }
 
 /*
+
+        <img className="exampleer" src="https://i.postimg.cc/vmQfCVr6/0954-CFAF-BFE4-42-A3-BDDF-84-C940473277-1-201-a.jpg" align="center" alt="capstone1"></img>
+
  serving as a virtual tutor in The Virtual Classroom Detention
  
 Simply put, the Virtual Classroom / Detention is where the non-traditional students like me get sent to go supplement our clasroom education after being kicked out
