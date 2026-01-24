e8e44f73544e4b9e1214262c4967bc5ba86afaa3
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 12:30:10 -0500

    Update contact.js

--- website/src/components/contact.js
+++ website/src/components/contact.js
@@ -1,9 +1,12 @@
 export default function Contact() {
     return (
         <div className="contact">
-          <a className="msglink" href="https://www.linkedin.com/in/wrongaddress/"> LinkedIn </a>
-          <a className="msglink" href="https://docs.google.com/forms/d/e/1FAIpQLSfx8rrHRvZVKwNzhOCqloYEAXB0cLLrBxR9DBzJG3VFVsp2Fg/viewform?usp=sf_link"> Send me a message! </a>
-          <h2 className="black"> techteachertrey@gmail.com, CEO@theemstech.com </h2>
+          <a className="msglink" href="https://www.linkedin.com/in/treweathers/"> LinkedIn </a>
+          <h2 className="black"> treweathers@proton.me </h2>
         </div>
       )
     }
+/*
+          <a className="msglink" href="https://docs.google.com/forms/d/e/1FAIpQLSfx8rrHRvZVKwNzhOCqloYEAXB0cLLrBxR9DBzJG3VFVsp2Fg/viewform?usp=sf_link"> Send me a message! </a>
+
+*/
