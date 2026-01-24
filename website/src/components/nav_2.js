7451f7b1b3c4307a57e48cd1bf71f3d63206784b
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 08:54:57 -0500

    Update nav_2.js

--- website/src/components/nav_2.js
+++ website/src/components/nav_2.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+/* import { Link } from "react-router-dom";
 //Skills, Projects
 //<Link className="link black" to={Skills}> Skills </Link>
 export default function Navs({Education}) {
@@ -10,3 +10,4 @@ export default function Navs({Education}) {
     </div>
   );
 }
+*/
