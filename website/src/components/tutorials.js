eae3cba0a73f20f7f167eb67e382a474db47eebd
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 13:01:29 -0500

    Update tutorials.js

--- website/src/components/tutorials.js
+++ website/src/components/tutorials.js
@@ -4,11 +4,15 @@ export default function Tutorials() {
   return (
       <div>
 
-        <h3>While stalking me casually perusing my portfolio, maybe you'll stumble upon something that you're interested in trying out.
-          Here, you will find a few walk-throughs for some of the nifty things i've come across to do in order to gain my desired skill set.</h3>
+        <h3> Helpful Videos for Beginners </h3>
 
         <Nave YoutubeEmbed="/hardware"  YoutubeEmbed3="/production" YoutubeEmbed2="/neural_networks"/>
         
       </div>
     )
   }
+
+/*
+While stalking me casually perusing my portfolio, maybe you'll stumble upon something that you're interested in trying out.
+Here, you will find a few walk-throughs for some of the nifty things i've come across to do in order to gain my desired skill set
+*/
