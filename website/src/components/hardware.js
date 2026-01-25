1da2647f6e9e91fddab1177892c6b666d4eaa7e6
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 19:06:00 -0500

    Update hardware.js

--- website/src/components/hardware.js
+++ website/src/components/hardware.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 const YoutubeEmbed = ({embedId}) => (
   <div>
+<p></p>
 <iframe className="video-big" width="1121" height="631" src="https://www.youtube.com/embed/shN0HNnvaoM" title="How I&#39;d Learn Front End Web Development (If I Could Start Over)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 <p></p>
 <iframe className="video-big" width="1121" height="631" src="https://www.youtube.com/embed/qyHyFsT7Hig" title="Frontend Development explained in 2 minutes // Tech in 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
