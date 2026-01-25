45b0b59283593ec71cb8c8cf981a0485772b3457
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 19:48:42 -0500

    Update App.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";  //useState, useEffect } 
 import { Routes, Route } from 'react-router-dom';
-import { Nav, Home, Aboutme, Algorithms, Codes, Contact, Data, Education, Services, Tutorials, YoutubeEmbed, YoutubeEmbed2, YoutubeEmbed3} from './components/index.js'
+import { Nav, Home, Aboutme, Algorithms, Codes, Contact, Data, Education, Skills, Tutorials, YoutubeEmbed, YoutubeEmbed2, YoutubeEmbed3} from './components/index.js'
 import './App.css';
 
 //use HashRouter to fix manual reloading? - make sure to import up top, change keywords below, and change from BrowserRouter in index.js
@@ -18,8 +18,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Nav Home="/" Aboutme="/aboutme" Algorithms="/algorithms" Codes="/codes" Contact="/contact" Data="/data" Education="/education"  Services="/services"
-      Tutorials="/tutorials" YoutubeEmbed="/hardware" YoutubeEmbed2="neural_networks" YoutubeEmbed3="/production"/>
+      <Nav Home="/" Aboutme="/aboutme" Algorithms="/algorithms" Codes="/codes" Contact="/contact" Data="/data" Education="/education"  Skills="/skills"
+      Learn="/learn" YoutubeEmbed="/frontend" YoutubeEmbed2="backend" YoutubeEmbed3="/ai"/>
 
         <Routes>
         <Route path="/" element={<Home />}/>
@@ -29,11 +29,11 @@ function App() {
         <Route path="/codes" element={<Codes />}/>
         <Route path="/contact" element={<Contact />}/> 
         <Route path="/education" element={<Education />}/>
-        <Route path="/services" element={<Services />}/> 
+        <Route path="/skills" element={<Skills />}/> 
         <Route path="/tutorials" element={<Tutorials />} />
-        <Route path="/hardware" element={<YoutubeEmbed embedId="Utwnm2kjYAM" embedId3="4SA6t_tcFQA"/>} />
-        <Route path="/neural_networks" element={<YoutubeEmbed2 embedId2="_c_x8A3mNDk"/>} />
-        <Route path="/production" element={<YoutubeEmbed3 embedId="NgWujOrCZFo"/>} />
+        <Route path="/frontend" element={<YoutubeEmbed embedId="Utwnm2kjYAM" embedId3="4SA6t_tcFQA"/>} />
+        <Route path="/backend" element={<YoutubeEmbed2 embedId2="_c_x8A3mNDk"/>} />
+        <Route path="/ai" element={<YoutubeEmbed3 embedId="NgWujOrCZFo"/>} />
         </Routes>
 
     </div>
