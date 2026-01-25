ed2843377c3fad6477269579e95462268076b73b
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 19:28:58 -0500

    Update production.js

--- website/src/components/production.js
+++ website/src/components/production.js
@@ -3,9 +3,27 @@ import PropTypes from "prop-types";
 
 const YoutubeEmbed3 = ({embedId}) => (
   <div>
-  
-<iframe className="video-big" width="1121" height="631" src="https://www.youtube.com/embed/qYNweeDHiyU" title="AI, Machine Learning, Deep Learning and Generative AI Explained" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-<iframe className="video-big" width="1121" height="631" src="https://www.youtube.com/embed/z-EtmaFJieY" title="Machine Learning &amp; Artificial Intelligence: Crash Course Computer Science #34" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  <p></p>
+  <iframe className="video-big"
+    width="1121"
+    height="631"
+    src="https://www.youtube.com/embed/qYNweeDHiyU"
+    title="AI, Machine Learning, Deep Learning and Generative AI Explained"
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen>
+  </iframe>
+  <p></p>
+  <iframe className="video-big"
+    width="1121"
+    height="631"
+    src="https://www.youtube.com/embed/z-EtmaFJieY"
+    title="Machine Learning &amp; Artificial Intelligence: Crash Course Computer Science #34"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen>
+  </iframe>
   </div>
 );
 
