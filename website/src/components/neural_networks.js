b2d4786e6850a48f8cb22a74c2d9f6213f60ea56
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-24 19:20:18 -0500

    Update neural_networks.js

--- website/src/components/neural_networks.js
+++ website/src/components/neural_networks.js
@@ -4,10 +4,28 @@ import PropTypes from "prop-types";
 const YoutubeEmbed2 = ({embedId3, embedId2}) => (
 <div>
     <p></p>
-<iframe className="video-big" width="1121" height="631" src="https://www.youtube.com/embed/cbSrsYiRamo" title="Backend Development explained in 2 minutes // Tech in 2" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe className="video-big"
+        width="1121"
+        height="631"
+        src="https://www.youtube.com/embed/cbSrsYiRamo"
+        title="Backend Development explained in 2 minutes // Tech in 2"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen>
+    </iframe>
     <p></p>
     
-<iframe className="video-big" width="1121" height="631" src="https://www.youtube.com/embed/4r6WdaY3SOA" title="How The Backend Works" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe className="video-big"
+        width="1121"
+        height="631"
+        src="https://www.youtube.com/embed/4r6WdaY3SOA"
+        title="How The Backend Works"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen>
+    </iframe>
   </div>
 );
 
