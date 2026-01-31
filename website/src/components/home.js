24da6f2a3537317a0e0173f9a70851ae5e9e8561
Tre Weathers <117105883+treweathers@users.noreply.github.com>
2026-01-31 18:19:11 -0500

    Update home.js

--- website/src/components/home.js
+++ website/src/components/home.js
@@ -2,13 +2,13 @@ export default function Home() {
   return (
       <div className="homepage">
           <h1 id="title-white"> Tre Weathers </h1>
-          <img className="example" src="https://i.postimg.cc/xjvnSwnq/mx05abasr3s01.jpg" align="center" alt="treytheurkel"></img>
           <h1> Computer Science Portfolio </h1>
       </div>
     )
   }
 
   /*
+  <img className="example" src="https://i.postimg.cc/xjvnSwnq/mx05abasr3s01.jpg" align="center" alt="treytheurkel"></img>
         <h3> Click the images below for the inside scoop! </h3>
         <a href="https://brave-glacier-00e751b0f.3.azurestaticapps.net/projects"> <img className="robot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </a>
         <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </a>
